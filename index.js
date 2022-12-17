108e67693b7306c6295718f196064f95260ce861
Cupu-Boy <fajrir357@gmail.com>
2022-12-17 17:23:35 +0700

    minor fix

--- index.js
+++ index.js
@@ -8,17 +8,11 @@ require("dotenv").config({
 const app = express();
 const port = process.env.PORT;
 
-app.use('/', require('./src/routes'))
-
-app.get('/', (req, res) => {
-    res.json({
-        success: true,
-        message: 'berhasil'
-    })
-})
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use("/", require("./src/routes"));
 
 app.get("/", (req, res) => {
   res.json({
