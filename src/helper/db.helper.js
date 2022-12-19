0839ceb243b5decdc7bd16af703a8961152288d5
Rio Prayoga <yoprayogap@gmail.com>
2022-12-19 16:11:57 +0700

    db config

--- src/helper/db.helper.js
+++ src/helper/db.helper.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
 
 const db = new Pool({
-  connectionString: "postgresql:postgres:1@localhost:5432/peworld_app",
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgresql:postgres:1@localhost:5432/peworld_app",
 });
 
 module.exports = db;
-
-// process.env.DATABASE_URL
