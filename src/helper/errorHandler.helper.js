8da5ea683decba4d6e01508553da9b1f6cd9e211
Rio Prayoga <yoprayogap@gmail.com>
2022-12-19 07:32:07 +0700

    fix minor

--- src/helper/errorHandler.helper.js
+++ src/helper/errorHandler.helper.js
@@ -11,22 +11,22 @@ exports.errorHandler = (error, res) => {
       message: "Phone number already used",
     });
   }
-  if (err.message.includes('backend error:')){
-    if (err.message.includes('code_expired')) {
+  if (error.message.includes("backend error:")) {
+    if (error.message.includes("code_expired")) {
       return res.status(400).json({
         succes: false,
-        message: "Code Expired!"
+        message: "Code Expired!",
       });
     }
-    if (err.message.includes('notfound_code_request')) {
+    if (error.message.includes("notfound_code_request")) {
       return res.status(400).json({
         succes: false,
-        message: "Reset Request not found!"
+        message: "Reset Request not found!",
       });
     }
-  };
+  }
   return res.status(500).json({
     succes: false,
-    message: 'Something happend in our backend'
+    message: "Something happend in our backend",
   });
 };
