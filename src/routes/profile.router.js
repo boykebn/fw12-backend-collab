4296a3d7065e1a49eb16d8e00764ed41affe065b
Rio Prayoga <yoprayogap@gmail.com>
2022-12-20 19:25:13 +0700

    menambahkan params user id

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -6,7 +6,7 @@ const {
   readPortofolio,
 } = require("../controller/profile.controller");
 
-profile.get("/", readProfile);
+profile.get("/:id", readProfile);
 profile.get("/experiences", readExperience);
 profile.get("/portofolio", readPortofolio);
 
