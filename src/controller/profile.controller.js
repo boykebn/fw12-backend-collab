ceb8d8d9b2066f1d86970da12deb2edccf2711b9
Rio Prayoga <yoprayogap@gmail.com>
2022-12-21 13:11:17 +0700

    menambahkan get profile by token

--- src/controller/profile.controller.js
+++ src/controller/profile.controller.js
@@ -15,7 +15,25 @@ exports.readProfile = (req, res) => {
 
     return res.status(200).json({
       success: true,
-      message: "Get profile user",
+      message: "Get profile user by id",
+      results: data.rows[0],
+    });
+  });
+};
+
+exports.readProfileByToken = (req, res) => {
+  console.log(req.userData);
+  selectProfile(req.userData.id, (err, data) => {
+    if (data.rows.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Profile not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Get profile user by token",
       results: data.rows[0],
     });
   });
--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -4,8 +4,12 @@ const {
   readProfile,
   readExperience,
   readPortofolio,
+  readProfileByToken,
 } = require("../controller/profile.controller");
 
+const authMiddleware = require("../middleware/auth.middleware");
+
+profile.get("/myAccount", authMiddleware, readProfileByToken);
 profile.get("/:id", readProfile);
 profile.get("/experiences/:id", readExperience);
 profile.get("/portofolio/:id", readPortofolio);
